54c5ac28692f53c74399c6e92e714d6b2ad7b746
Danilo Romano <risadex@gmail.com>
2022-04-14 10:39:30 -0300

    added confirmation route redirects
    Signed-off-by: Danilo Romano <risadex@gmail.com>

--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:notesapp/views/register_view.dart';
 
 class VerifyEmailView extends StatefulWidget {
+  static const route = '/verify-email/';
   const VerifyEmailView({Key? key}) : super(key: key);
 
   @override
@@ -13,17 +15,31 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(('Verify email')),
+        title: const Text('Verify email'),
       ),
       body: Column(
         children: [
-          const Text('Please verify your email'),
+          const Text(
+              "We've sent you an email verification. Please open it to verify your account."),
+          const Text(
+              "You you haven't received the email yet, press the button bellow"),
           TextButton(
-              onPressed: () async {
-                final user = FirebaseAuth.instance.currentUser;
-                await user?.sendEmailVerification();
-              },
-              child: const Text('Verify your email'))
+            onPressed: () async {
+              final user = FirebaseAuth.instance.currentUser;
+              await user?.sendEmailVerification();
+            },
+            child: const Text('Verify your email'),
+          ),
+          TextButton(
+            onPressed: () async {
+              FirebaseAuth.instance.signOut();
+              Navigator.of(context).pushNamedAndRemoveUntil(
+                RegisterView.route,
+                (route) => false,
+              );
+            },
+            child: const Text('Restart'),
+          ),
         ],
       ),
     );
