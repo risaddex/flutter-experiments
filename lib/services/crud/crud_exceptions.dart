e63187e2b1ea5f166fa6d90e01fd01d86fc44085
Danilo Romano <risadex@gmail.com>
2022-04-14 18:07:29 -0300

    step 11
    Signed-off-by: Danilo Romano <risadex@gmail.com>

---
+++ lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,13 @@
+class DatabaseAlreadyOpenedException implements Exception {}
+
+class UnableToGetDocumentsDirectory implements Exception {}
+
+class DatabaseIsNotOpenException implements Exception {}
+
+class CouldNotDeleteException<T> implements Exception {}
+
+class AlreadyExistsException<T> implements Exception {}
+
+class CouldNotFindException<T> implements Exception {}
+
+class CouldNotUpdateException<T> implements Exception {}
